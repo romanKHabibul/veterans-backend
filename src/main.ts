dec4d716507f94b25de36daf7ca483eba91cfb37
unknown <horizonXD222@yandex.ru>
2023-05-29 15:38:38 +0300

    fgfd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.setGlobalPrefix("api")
-  await app.listen(1000, "0.0.0.0", () => console.log("server♥"));
+  await app.listen(3000, "0.0.0.0", () => console.log("server♥"));
 }
 bootstrap();
 
